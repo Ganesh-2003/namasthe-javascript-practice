62d608e482fc145b92500b80c897e7cb770e19f0
Ganesh-2003 <ganeeshpuppala@gmail.com>
2025-12-25 08:36:07 +0530

    Intial commit

---
+++ #14_CallBack_Functions/index.js
@@ -0,0 +1,14 @@
+//What is Callback function in Javascript
+
+setTimeout(function (){
+    console.log("timer");
+},5000)
+
+function x(y) {
+    console.log("x");
+    y();
+}
+
+x(function y(){
+    console.log("y");
+})
